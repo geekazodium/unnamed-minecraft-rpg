741578026f9f26c35a826ab405e038f30e91fa61
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2023-01-23 22:26:07 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-//import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
-
 repositories{
   flatDir {
     dirs("localLibs")
@@ -14,7 +12,6 @@ plugins {
   id("io.papermc.paperweight.userdev") version "1.3.8"
   id("xyz.jpenilla.run-paper") version "1.1.0" // Adds runServer and runMojangMappedServer tasks for testing
   id("com.github.johnrengelman.shadow") version "7.1.2"
-  //id("net.minecrell.plugin-yml.bukkit") version "0.5.2" // Generates plugin.yml
 }
 
 group = "com.geekazodium"
@@ -22,7 +19,6 @@ version = "1.0-SNAPSHOT"
 description = "idek"
 
 java {
-  // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
   toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
@@ -39,16 +35,12 @@ dependencies {
 }
 
 tasks {
-  // Configure reobfJar to run when invoking the build task
   assemble {
     dependsOn(reobfJar)
   }
 
   compileJava {
     options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
-
-    // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
-    // See https://openjdk.java.net/jeps/247 for more information.
     options.release.set(17)
     options.compilerArgs.add("-Xlint:deprecation")
   }
@@ -67,11 +59,3 @@ tasks {
   }
    */
 }
-/*
-bukkit {
-  load = BukkitPluginDescription.PluginLoadOrder.STARTUP
-  main = "com.geekazodium.cavernsofamethyst.Main"
-  apiVersion = "1.18"
-  authors = listOf("Geekazodium")
-}
-*/
